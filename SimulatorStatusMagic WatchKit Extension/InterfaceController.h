820f9e0ab3d237e245d8747e3680f7725c9981e6
Sebastian Hagedorn <sebastian@iosphere.de>
2015-06-02 16:56:25 +0200

    Adds Apple Watch app and extension for easier testing

---
+++ SimulatorStatusMagic WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  SimulatorStatusMagic WatchKit Extension
+//
+//  Created by Hagi on 02/06/15.
+//  Copyright (c) 2015 Shiny Development. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
